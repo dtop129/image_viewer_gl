6ab53a4d24cad5da0eb1bc97f43291b0d58a3950
dtop <dantop912@gmail.com>
2024-03-05 01:26:33 +0100

    Removed openGL debug, force images to be unique

--- app.h
+++ app.h
@@ -16,45 +16,6 @@
 #include "shader.h"
 #include "loader_thread.h"
 
-void message_callback(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, GLchar const* message, void const* user_param)
-{
-	auto const src_str = [source]() {
-		switch (source)
-		{
-		case GL_DEBUG_SOURCE_API: return "API";
-		case GL_DEBUG_SOURCE_WINDOW_SYSTEM: return "WINDOW SYSTEM";
-		case GL_DEBUG_SOURCE_SHADER_COMPILER: return "SHADER COMPILER";
-		case GL_DEBUG_SOURCE_THIRD_PARTY: return "THIRD PARTY";
-		case GL_DEBUG_SOURCE_APPLICATION: return "APPLICATION";
-		default: return "OTHER";
-		}
-	}();
-
-	auto const type_str = [type]() {
-		switch (type)
-		{
-		case GL_DEBUG_TYPE_ERROR: return "ERROR";
-		case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: return "DEPRECATED_BEHAVIOR";
-		case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR: return "UNDEFINED_BEHAVIOR";
-		case GL_DEBUG_TYPE_PORTABILITY: return "PORTABILITY";
-		case GL_DEBUG_TYPE_PERFORMANCE: return "PERFORMANCE";
-		case GL_DEBUG_TYPE_MARKER: return "MARKER";
-		default: return "OTHER";
-		}
-	}();
-
-	auto const severity_str = [severity]() {
-		switch (severity) {
-		case GL_DEBUG_SEVERITY_NOTIFICATION: return "NOTIFICATION";
-		case GL_DEBUG_SEVERITY_LOW: return "LOW";
-		case GL_DEBUG_SEVERITY_MEDIUM: return "MEDIUM";
-		case GL_DEBUG_SEVERITY_HIGH: return "HIGH";
-		default: return "OTHER";
-		}
-	}();
-	std::cout << src_str << ", " << type_str << ", " << severity_str << ", " << id << ": " << message << '\n';
-}
-
 class image_viewer
 {
 private:
@@ -73,8 +34,8 @@ private:
 	std::vector<std::future<glm::ivec2>> loading_image_sizes;
 	std::vector<int> image_types;
 
-	std::unordered_map<int, std::vector<std::pair<int, std::future<int>>>> loading_image_types;
 	std::map<int, std::vector<int>> tags_indices; //tag -> vector of indices pointing to image_paths/image_sizes
+	std::unordered_map<int, std::vector<std::pair<int, std::future<int>>>> loading_image_types;
 	std::unordered_map<int, std::vector<int>> page_numbers; //tag -> vector of indices(referring to tags_indices) indicating the page index
 
 	texture_load_thread loader_pool;
@@ -210,6 +171,12 @@ private:
 				if (!std::filesystem::exists(image_path))
 				{
 					std::cerr << image_path << " not found" << std::endl;
+					continue;
+				}
+				if (std::find(image_paths.begin(), image_paths.end(), image_path)
+						!= image_paths.end())
+				{
+					std::cerr << image_path << " already present" << std::endl;
 					continue;
 				}
 
@@ -581,14 +548,10 @@ public:
 					app->on_key(key, action);
 				});
 
+		init_GLresources();
+
 		std::ios_base::sync_with_stdio(false);
 		std::cin.tie(NULL);
-
-		glEnable(GL_DEBUG_OUTPUT);
-		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
-		glDebugMessageCallback(message_callback, nullptr);
-		glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_NOTIFICATION, 0, nullptr, GL_FALSE);
-		init_GLresources();
 	}
 
 	void run()
